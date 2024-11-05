ab9001e4e0e7ad6f290b0416016ff77f4a04a3a1
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2024-11-05 15:50:56 +0900

    あ

--- Give Up!/Assets/Scripts/Player/PlayerController.cs
+++ Give Up!/Assets/Scripts/Player/PlayerController.cs
@@ -72,7 +72,6 @@ public class PlayerController : MonoBehaviour
 
     public void Knockback(Vector3 direction, float force)
     {
-        moveDirection += direction * force; // ノックバック方向と強さを合成
-        moveDirection.y = Mathf.Max(moveDirection.y, force * 0.5f); // Y軸方向にも持ち上げるように設定
+        hips.AddForce(direction.normalized * force, ForceMode.Impulse);
     }
 }
