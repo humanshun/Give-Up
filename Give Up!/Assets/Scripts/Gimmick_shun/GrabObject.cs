b3606ed94fa4d5bff3c1d2947212757bbc46e53e
humanshun <human.kagoshima.shindomeshunsuke@gmail.com>
2024-11-22 16:15:14 +0900

    オブジェクト移動の修正と、フォント追加

--- Give Up!/Assets/Scripts/Gimmick_shun/GrabObject.cs
+++ Give Up!/Assets/Scripts/Gimmick_shun/GrabObject.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class GrabObject : MonoBehaviour
 {
     private Rigidbody rb;
+    private GrabLeftHand grabLeftHand;
+    private GrabRightHand grabRightHand;
     private FixedJoint fixedJoint;
     void Start()
     {
         // Rigidbodyコンポーネントを取得
         rb = GetComponent<Rigidbody>();
     }
-    void Update()
-    {
-        // 毎フレームでのチェックも可能
-        if (fixedJoint == null)
-        {
-            fixedJoint = GetComponent<FixedJoint>();
-        }
 
-        if (fixedJoint != null)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Hand"))
         {
-            rb.useGravity = true;
-            rb.isKinematic = false;
+
+            GrabLeftHand grabLeftHand = collision.gameObject.GetComponent<GrabLeftHand>();
+            GrabRightHand grabRightHand = collision.gameObject.GetComponent<GrabRightHand>();
+
+            // 毎フレームでのチェックも可能
+            if (fixedJoint == null)
+            {
+                fixedJoint = GetComponent<FixedJoint>();
+            }
+
+            if (fixedJoint != null)
+            {
+                rb.useGravity = true;
+                rb.isKinematic = false;
+            }
         }
-        else
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (fixedJoint != null)
         {
             rb.useGravity = false;
             rb.isKinematic = true;
